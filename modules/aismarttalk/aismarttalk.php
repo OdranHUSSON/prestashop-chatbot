feea4323f755dff1539b6b5934754c8e147214eb
Pierre-Antoine Caillaud <pierre-antoine.caillaud@outlook.com>
2025-11-16 20:20:10 +0100

    prestashop 9

--- modules/aismarttalk/aismarttalk.php
+++ modules/aismarttalk/aismarttalk.php
@@ -35,7 +35,7 @@ class AiSmartTalk extends Module
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
             'min' => '1.7.0.0',
-            'max' => '8.99.99',
+            'max' => '9.99.99',
         ];
         $this->bootstrap = true;
 
